4687c5296c5cee28119029756b19d8c97ae1e92c
Hans Evan Tatipata <thansevan@gmail.com>
2022-06-15 00:21:07 +0700

    erase the private data

--- koneksi.php
+++ koneksi.php
@@ -5,15 +5,6 @@ $user = "root";
 $pass = "";
 $db = "bemfikti_bistalk";
 
-// $host ="localhost"; //host server
-// $user ="bemfikti_ehek"; //user login phpMydmin
-// $pass ="ehek123"; //pass login phpMyAdmin
-// $db ="bemfikti_font"; //name database
-
-// $host ="localhost"; 
-// $user ="bemfikti_basket"; 
-// $pass ="QwertyAlif4321"; 
-// $db ="bemfikti_bball"; 
 $conn = mysqli_connect($host, $user, $pass, $db) or die("koneksi gagal");
 
 //untuk HTTPS
